f5b7fba3f8e1d0364e7df9788e2ff86358ef9e3a
mekh <smajev@protonmail.com>
2026-03-26 19:58:18 +0100

    EquipmentService: implemented
    Signed-off-by: mekh <smajev@protonmail.com>

--- StudentRentalShop/equipment/service/EquipmentService.cs
+++ StudentRentalShop/equipment/service/EquipmentService.cs
@@ -1,6 +1,58 @@
-﻿namespace StudentRentalShop.service;
+﻿using DefaultNamespace;
+
+namespace StudentRentalShop.service;
 
 public class EquipmentService
 {
+    private static EquipmentService _instance = null;
+    
+    List<Equipment> _equipments;
+    
+    private EquipmentService()
+    {
+        _equipments = new List<Equipment>();
+    }
+
+    public static EquipmentService getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new EquipmentService();
+        }
+        return _instance;
+    }
+
+    public IReadOnlyList<Equipment> GetEquipments()
+    {
+        return _equipments;
+    }
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipments.Add(equipment);
+    }
+
+    public void rentEquipment(string name)
+    {
+        GetEquipment(name).status = EquipmentStatus.Available;
+    }
+
+    public void returnEquipment(string name)
+    {
+        GetEquipment(name).status = EquipmentStatus.Available;
+    }
+    
+    private Equipment GetEquipment(string name)
+    {
+        foreach (Equipment equipment in _equipments)
+        {
+            if (equipment.name == name)
+            {
+                return equipment;
+            }
+        }
+        throw new KeyNotFoundException("Equipment not found");
+    }
+    
     
 }
