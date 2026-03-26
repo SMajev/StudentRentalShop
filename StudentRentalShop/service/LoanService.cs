a0b99990a3dc84b9e13c851349ea8c510a0cda13
mekh <smajev@protonmail.com>
2026-03-26 19:20:48 +0100

    LoanService: init
    Signed-off-by: mekh <smajev@protonmail.com>

---
+++ StudentRentalShop/service/LoanService.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentRentalShop.service;
+
+public class LoanService
+{
+    private static LoanService _instance;
+    
+    // Singleton private constructor
+    private LoanService()
+    {
+        
+    }
+
+    public static LoanService getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new LoanService();
+        }
+        return _instance;
+    }
+    
+}
