8967abadb01ea8949fb9724de889309e11036f93
mekh <smajev@protonmail.com>
2026-03-27 17:51:20 +0100

    User, Student, Emplloyee implementation

---
+++ StudentRentalShop/user/model/User.cs
@@ -0,0 +1,31 @@
+﻿using DefaultNamespace;
+
+namespace StudentRentalShop.user.model;
+
+public abstract class User
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    List<Equipment> rentedEquipments;
+
+    public abstract int maxLoans { get; }
+    
+    public User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        rentedEquipments = new List<Equipment>();
+    }
+    
+    public void addEquipmentToAccount(Equipment equipment)
+    {
+        if (rentedEquipments.Count >= maxLoans) throw  new Exception("Maximum number of equipments exceeded");
+        rentedEquipments.Add(equipment);
+    }
+
+    public void removeEquipmentFromAccount(Equipment equipment)
+    {
+        rentedEquipments.Remove(equipment);
+    }
+}
