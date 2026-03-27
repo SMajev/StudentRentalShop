3a9650509eb8417b8807f68bae1e1869c1c53711
mekh <smajev@protonmail.com>
2026-03-27 18:17:39 +0100

    User Service, Dto, Factory Base

---
+++ StudentRentalShop/user/service/UserService.cs
@@ -0,0 +1,32 @@
+﻿using StudentRentalShop.user.model;
+
+namespace StudentRentalShop.user;
+
+public class UserService
+{
+    private static UserService Instance { get; set; } = null;
+    
+    private static List<User> users;
+
+
+    private UserService()
+    {
+    }
+
+    public static UserService GetInstance()
+    {
+        Instance ??= new UserService();
+        return Instance;
+    }
+
+
+    public IReadOnlyList<User> GetUsers()
+    {
+        return users;
+    }
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+}
